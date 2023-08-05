bdc79418efcefdfae0343908b0b2c56debb4cf36
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-08-05 19:49:24 +1000

    remove special handling for iterate/collect in ui
    woohoo!

--- invokeai/frontend/web/src/features/nodes/components/fields/CollectionItemInputFieldComponent.tsx
+++ invokeai/frontend/web/src/features/nodes/components/fields/CollectionItemInputFieldComponent.tsx
@@ -3,7 +3,6 @@ import {
   CollectionItemInputFieldValue,
 } from 'features/nodes/types/types';
 import { memo } from 'react';
-import { FaAddressCard } from 'react-icons/fa';
 import { FieldComponentProps } from './types';
 
 const CollectionItemInputFieldComponent = (
@@ -12,7 +11,7 @@ const CollectionItemInputFieldComponent = (
     CollectionItemInputFieldTemplate
   >
 ) => {
-  return <FaAddressCard />;
+  return null;
 };
 
 export default memo(CollectionItemInputFieldComponent);
--- invokeai/frontend/web/src/features/nodes/util/parseSchema.ts
+++ invokeai/frontend/web/src/features/nodes/util/parseSchema.ts
@@ -4,7 +4,6 @@ import {
   InputFieldTemplate,
   InvocationSchemaObject,
   InvocationTemplate,
-  OutputFieldTemplate,
   isInvocationFieldSchema,
   isInvocationSchemaObject,
 } from '../types/types';
@@ -38,100 +37,40 @@ export const parseSchema = (
     if (isInvocationSchemaObject(schema)) {
       const type = schema.properties.type.default;
       const title = schema.ui?.title ?? schema.title.replace('Invocation', '');
-      console.log(schema);
       const tags = schema.ui?.tags ?? [];
+      const description = schema.description ?? '';
 
-      const inputs: Record<string, InputFieldTemplate> = {};
+      const inputs = reduce(
+        schema.properties,
+        (inputsAccumulator, property, propertyName) => {
+          if (
+            !RESERVED_FIELD_NAMES.includes(propertyName) &&
+            isInvocationFieldSchema(property) &&
+            !property.ui_hidden
+          ) {
+            const field = buildInputFieldTemplate(
+              schema,
+              property,
+              propertyName
+            );
 
-      if (type === 'collect') {
-        const itemProperty = schema.properties.item as InvocationSchemaObject;
-        inputs.item = {
-          type: 'CollectionItem',
-          name: 'item',
-          description: itemProperty.description ?? '',
-          title: 'Collection Item',
-          input: 'connection',
-          default: undefined,
-          ui_hidden: false,
-          required: true,
-        };
-      } else if (type === 'iterate') {
-        const itemProperty = schema.properties
-          .collection as InvocationSchemaObject;
-        inputs.collection = {
-          type: 'Collection',
-          name: 'collection',
-          title: itemProperty.title ?? '',
-          default: [],
-          description: itemProperty.description ?? '',
-          input: 'connection',
-          ui_hidden: false,
-          required: true,
-        };
-      } else {
-        reduce(
-          schema.properties,
-          (inputsAccumulator, property, propertyName) => {
-            if (
-              !RESERVED_FIELD_NAMES.includes(propertyName) &&
-              isInvocationFieldSchema(property)
-            ) {
-              const field = buildInputFieldTemplate(
-                schema,
-                property,
-                propertyName
-              );
-
-              if (field) {
-                inputsAccumulator[propertyName] = field;
-              }
-            } else {
-              // console.warn('Unhandled INPUT property', property);
+            if (field) {
+              inputsAccumulator[propertyName] = field;
             }
-            return inputsAccumulator;
-          },
-          inputs
-        );
-      }
+          }
+          return inputsAccumulator;
+        },
+        {} as Record<string, InputFieldTemplate>
+      );
 
       const rawOutput = (schema as InvocationSchemaObject).output;
-      let outputs: Record<string, OutputFieldTemplate>;
-
-      if (type === 'iterate') {
-        // Special handling for iterate
-        const iterationOutput = openAPI.components?.schemas?.[
-          'IterateInvocationOutput'
-        ] as OpenAPIV3.SchemaObject;
-        outputs = {
-          item: {
-            name: 'item',
-            title: iterationOutput?.title ?? '',
-            description: iterationOutput?.description ?? '',
-            type: 'CollectionItem',
-          },
-        };
-      } else if (type === 'collect') {
-        // Special handling for collect
-        const collectionOutput = openAPI.components?.schemas?.[
-          'CollectInvocationOutput'
-        ] as OpenAPIV3.SchemaObject;
-        outputs = {
-          item: {
-            name: 'collection',
-            title: collectionOutput?.title ?? '',
-            description: collectionOutput?.description ?? '',
-            type: 'Collection',
-          },
-        };
-      } else {
-        outputs = buildOutputFieldTemplates(rawOutput, openAPI);
-      }
+      const outputs = buildOutputFieldTemplates(rawOutput, openAPI);
 
       const invocation: InvocationTemplate = {
         title,
         type,
         tags,
-        description: schema.description ?? '',
+        description,
         inputs,
         outputs,
       };
