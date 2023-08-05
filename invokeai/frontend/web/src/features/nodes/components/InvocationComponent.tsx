31deff64cdaf7619cd188838e9175a4d932337fa
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-08-05 14:18:17 +1000

    remove resizer

--- invokeai/frontend/web/src/features/nodes/components/InvocationComponent.tsx
+++ invokeai/frontend/web/src/features/nodes/components/InvocationComponent.tsx
@@ -1,17 +1,14 @@
-import { Collapse, Divider, Flex, Icon } from '@chakra-ui/react';
-import { FaExclamationCircle } from 'react-icons/fa';
-import { NodeProps } from 'reactflow';
-import { InvocationValue } from '../types/types';
-
+import { Divider, Flex, Icon } from '@chakra-ui/react';
 import { useAppSelector } from 'app/store/storeHooks';
 import { memo, useMemo } from 'react';
+import { FaExclamationCircle } from 'react-icons/fa';
+import { NodeProps } from 'reactflow';
 import { makeTemplateSelector } from '../store/util/makeTemplateSelector';
+import { InvocationValue } from '../types/types';
 import IAINodeHeader from './IAINode/IAINodeHeader';
 import IAINodeInputs from './IAINode/IAINodeInputs';
 import IAINodeOutputs from './IAINode/IAINodeOutputs';
-import IAINodeResizer from './IAINode/IAINodeResizer';
 import NodeWrapper from './NodeWrapper';
-import { AnimatePresence, motion } from 'framer-motion';
 
 export const InvocationComponent = memo((props: NodeProps<InvocationValue>) => {
   const { id: nodeId, data, selected } = props;
@@ -40,7 +37,6 @@ export const InvocationComponent = memo((props: NodeProps<InvocationValue>) => {
               _dark: { color: 'base.400' },
             }}
           ></Icon>
-          <IAINodeResizer />
         </Flex>
       </NodeWrapper>
     );
