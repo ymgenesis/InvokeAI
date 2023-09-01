346f3ca92a75975653f2dd586252204ac0ce7f6b
ymgenesis <janik.matthew@gmail.com>
2023-09-01 13:04:12 +0200

    even more default=none...
    - one last default=none I missed

--- invokeai/app/invocations/faceplace.py
+++ invokeai/app/invocations/faceplace.py
@@ -16,7 +16,7 @@ class FacePlaceInvocation(BaseInvocation):
     """FacePlace node to place the a bounded face from FaceOff back onto the original image"""
 
     bounded_image:   ImageField = InputField(description="The bounded image to be placed on the original image")
-    original_image:    ImageField = InputField(default=None, description="The original image to place the bounded image on")
+    original_image:    ImageField = InputField(description="The original image to place the bounded image on")
     downscale_factor:  int = InputField(default=2, description="Factor to downscale the bounded image before placing")
     x:                 int = InputField(default=0, description="The x coordinate (top left corner) to place on the original image")
     y:                 int = InputField(default=0, description="The y coordinate (top left corner) to place on the original image")
