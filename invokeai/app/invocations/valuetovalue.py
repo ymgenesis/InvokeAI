9c9f5d7db730899e7dae06e6a0da909c953a0d82
ymgenesis <janik.matthew@gmail.com>
2023-09-22 13:51:43 +0200

    add valuetovalue.py

---
+++ invokeai/app/invocations/valuetovalue.py
@@ -0,0 +1,53 @@
+from typing import Literal, Any
+import numpy as np
+from .baseinvocation import BaseInvocation, BaseInvocationOutput, InputField, OutputField, InvocationContext, invocation, invocation_output, UIType, Input
+
+@invocation_output("str_flt_int_output")
+class str_flt_int_output(BaseInvocationOutput):
+    """Base class for nodes that output a string, float, and integer"""
+
+    string_output: str = OutputField(description="The output string", title="String")
+    float_output: float = OutputField(description="The output float", title="Float")
+    integer_output: int = OutputField(description="The output integer", title="Integer")
+
+
+@invocation(
+    "value_to_value",
+    title="Value To Value",
+    tags=["math", "round", "integer", "float", "string", "convert"],
+    category="math",
+    version="1.0.0",
+)
+class ValueToValueInvocation(BaseInvocation):
+    """Outputs a given value as a string, float, or integer. Rounds in the case of float to integer."""
+
+    value: str = InputField(description="The value to pass forward")
+
+    round_multiple: int = InputField(default=1, ge=1, title="Multiple of", description="The multiple to round to for float to integer")
+    round_method: Literal["Nearest", "Floor", "Ceiling", "Truncate"] = InputField(
+        default="Nearest", description="The method to use for rounding"
+    )
+
+    def invoke(self, context: InvocationContext) -> str_flt_int_output:
+        try:
+            if float(self.value):        
+                if self.round_method == "Nearest":
+                    int_out=round(float(self.value) / self.round_multiple) * self.round_multiple
+                elif self.round_method == "Floor":
+                    int_out=np.floor(float(self.value) / self.round_multiple) * self.round_multiple
+                elif self.round_method == "Ceiling":
+                    int_out=np.ceil(float(self.value) / self.round_multiple) * self.round_multiple
+                else:  # self.method == "Truncate"
+                    int_out=int(float(self.value) / self.round_multiple) * self.round_multiple
+                string_out=str(self.value)
+                float_out=float(self.value)
+        except ValueError:
+            string_out=str(self.value)
+            float_out=0
+            int_out=0
+
+        return str_flt_int_output(
+            string_output=string_out,
+            float_output=float_out,
+            integer_output=int_out,
+        )
