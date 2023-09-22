bd5b33572a42db8c7c13e086c759a8ff54340f3e
ymgenesis <janik.matthew@gmail.com>
2023-09-22 13:55:01 +0200

    v2v black/flake8/isort/absolute import

--- invokeai/app/invocations/valuetovalue.py
+++ invokeai/app/invocations/valuetovalue.py
@@ -1,6 +1,17 @@
-from typing import Literal, Any
+from typing import Literal
+
 import numpy as np
-from .baseinvocation import BaseInvocation, BaseInvocationOutput, InputField, OutputField, InvocationContext, invocation, invocation_output, UIType, Input
+
+from invokeai.app.invocations.baseinvocation import (
+    BaseInvocation,
+    BaseInvocationOutput,
+    InputField,
+    InvocationContext,
+    OutputField,
+    invocation,
+    invocation_output,
+)
+
 
 @invocation_output("str_flt_int_output")
 class str_flt_int_output(BaseInvocationOutput):
@@ -23,28 +34,30 @@ class ValueToValueInvocation(BaseInvocation):
 
     value: str = InputField(description="The value to pass forward")
 
-    round_multiple: int = InputField(default=1, ge=1, title="Multiple of", description="The multiple to round to for float to integer")
+    round_multiple: int = InputField(
+        default=1, ge=1, title="Multiple of", description="The multiple to round to for float to integer"
+    )
     round_method: Literal["Nearest", "Floor", "Ceiling", "Truncate"] = InputField(
         default="Nearest", description="The method to use for rounding"
     )
 
     def invoke(self, context: InvocationContext) -> str_flt_int_output:
         try:
-            if float(self.value):        
+            if float(self.value):
                 if self.round_method == "Nearest":
-                    int_out=round(float(self.value) / self.round_multiple) * self.round_multiple
+                    int_out = round(float(self.value) / self.round_multiple) * self.round_multiple
                 elif self.round_method == "Floor":
-                    int_out=np.floor(float(self.value) / self.round_multiple) * self.round_multiple
+                    int_out = np.floor(float(self.value) / self.round_multiple) * self.round_multiple
                 elif self.round_method == "Ceiling":
-                    int_out=np.ceil(float(self.value) / self.round_multiple) * self.round_multiple
+                    int_out = np.ceil(float(self.value) / self.round_multiple) * self.round_multiple
                 else:  # self.method == "Truncate"
-                    int_out=int(float(self.value) / self.round_multiple) * self.round_multiple
-                string_out=str(self.value)
-                float_out=float(self.value)
+                    int_out = int(float(self.value) / self.round_multiple) * self.round_multiple
+                string_out = str(self.value)
+                float_out = float(self.value)
         except ValueError:
-            string_out=str(self.value)
-            float_out=0
-            int_out=0
+            string_out = str(self.value)
+            float_out = 0
+            int_out = 0
 
         return str_flt_int_output(
             string_output=string_out,
